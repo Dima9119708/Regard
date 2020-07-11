6a3f5199554a13a9fc6b3c547c703fc69cb32c0e
Dima9119708 <59973263+Dima9119708@users.noreply.github.com>
2020-07-11 11:21:51 +0300

    FINISH-DATA__BASE

--- src/js/Pages/MainPage.js
+++ src/js/Pages/MainPage.js
@@ -6,13 +6,12 @@ import { Footer } from '../Components/footer/Footer'
 import { LoginBar } from '../Components/loginBar/LoginBar'
 import { InterfacePages } from './InterfacePage'
 
-import base from '../base.json'
-
 export class MainPage extends InterfacePages {
 
   async render() {
 
-    const DATA = await base
+    const GET__DATA = await fetch('https://regard-ab2be.firebaseio.com/base.json')
+    const DATA = await GET__DATA.json()
 
     this.initComponent = new InitComponent(
       [HeaderTop, Header, Content, Footer, LoginBar],
--- src/js/core/preloader.js
+++ src/js/core/preloader.js
@@ -3,6 +3,7 @@ import { $ } from "./Dom"
 export function preloader() {
 
   const preloader = $.create('div', 'lds-roller')
+  preloader.setAttribute('data-preloader', true)
   preloader.innerHTML = `
     <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
   `
